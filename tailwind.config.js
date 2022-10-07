08c8385c2a7b0241436b8dcb7c09bcef167b61d2
Safiul-Azam <safiulazamriad@gmail.com>
2022-10-07 19:13:14 +0600

    added navbar from daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,23 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        foodOrdering: {
+          primary: "#079447",
+          secondary: "#EEAC00",
+          accent: "#1E1D23",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+};
