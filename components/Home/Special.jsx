5003bab96a922318629f0f9b980604f664f9ff0c
Safiul-Azam <safiulazamriad@gmail.com>
2022-10-08 12:47:14 +0600

    special services is done

--- components/Home/Special.jsx
+++ components/Home/Special.jsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import img2 from "../../images/spacial/2.png";
+import img3 from "../../images/spacial/3.png";
 import offer from "../../images/spacial/offer.png";
-function Spacial() {
+function Special() {
   return (
     <div className="my-24 w-full px-32">
       <div className="grid grid-cols-2 grid-flow-col container gap-x-5 mx-auto">
@@ -27,7 +28,7 @@ function Spacial() {
           </div>
         </div>
         <div
-          className="bg-[#292929] p-10 rounded-2xl"
+          className="bg-[#292929] p-10 rounded-2xl mb-6"
           style={{
             backgroundImage: `url(https://i.ibb.co/pds2HHx/2.png)`,
             backgroundPosition: "center",
@@ -35,24 +36,43 @@ function Spacial() {
           }}
         >
           <div className="w-1/2 space-y-6">
-            <h2 className="text-3xl font-bold text-white">Enjoy Our Delicious Item</h2>
+            <h2 className="text-3xl font-bold text-white">
+              Enjoy Our Delicious Item
+            </h2>
             <button className="text-pink-600 rounded-full btn bg-white">
               ORDER NOW
             </button>
           </div>
         </div>
-        <div className="">
-          <div>
-            <h2 className="text-4xl text-white">Enjoy Our Delicious Item</h2>
-            <button className="text-pink-600 rounded-full btn bg-white">
-              ORDER NOW
-            </button>
+        <div
+          className="rounded-2xl" 
+          style={{
+            backgroundImage: `linear-gradient(90.1deg, #ffeecc 38.57%, rgba(255, 238, 204, 0) 66.47%),url(https://i.ibb.co/qx48gXz/03.png)`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+        >
+          <div
+          className="p-10"
+            style={{
+              backgroundImage: `url( https://i.ibb.co/fChzNpF/3.png)`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="w-1/2 space-y-6">
+              <h2 className="text-3xl font-bold">
+                Enjoy Our Delicious Item
+              </h2>
+              <button className="text-pink-600 rounded-full btn bg-white">
+                ORDER NOW
+              </button>
+            </div>
           </div>
-          <Image src={img2} alt="" />
         </div>
       </div>
     </div>
   );
 }
 
-export default Spacial;
+export default Special;
