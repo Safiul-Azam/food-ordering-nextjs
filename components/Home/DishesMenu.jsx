bde27c5db0987bf678ff2f0bb61e2ecb5f0a118f
Safiul-Azam <safiulazamriad@gmail.com>
2022-10-11 09:20:42 +0600

    button problem fixed in popular dishes

--- components/Home/DishesMenu.jsx
+++ components/Home/DishesMenu.jsx
@@ -9,7 +9,6 @@ function DishesMenu() {
       .then((data) => setFoods(data));
   }, []);
   const [filterByBtn, setFilterByBtn] = useState([]);
-  const [foodName, setFoodName] = useState("");
   const [active, setActive] = useState("all");
 
   const btnByFoodName = [
@@ -22,16 +21,19 @@ function DishesMenu() {
   ];
   const handleClick = (btn) => {
     setFilterByBtn(foods.filter((food) => food.category.toLowerCase() === btn));
+    if(btn === "all"){
+      setFilterByBtn(foods)
+    }
     // setFoodName(foods.find((food) => food?.category?.toLowerCase() === btn));
     setActive(btn);
   };
   return (
     <>
-      <div>
-        <h2 className="text-center text-4xl text-black font-extrabold mb-12">
+      <div className="mt-16">
+        <h2 className="text-center text-4xl text-black font-extrabold mb-10">
           Popular dishes
         </h2>
-        <div className="flex justify-center space-x-10 mb-10">
+        <div className="flex justify-center space-x-10 mb-8">
           {btnByFoodName.map((btn) => (
             <div
               className={`border flex justify-center hover:border-secondary w-28 p-2 rounded-3xl font-bold hover:duration-200 hover:ease-in-out ease-in-out duration-200 ${
--- components/Home/PopularDishes.jsx
+++ components/Home/PopularDishes.jsx
@@ -14,47 +14,96 @@ function PopularDishes({ filterByBtn }) {
   }, []);
   return (
     <>
-      {/* <DishesMenu /> */}
-      <div className="container mx-auto grid grid-cols-4 gap-8">
-        {foods.map((food) => {
-          return (
-            <div key={food.id} className="rounded-3xl border">
-              <div className="m-2 mb-0 px-3 pt-3 pb-7 rounded-3xl hover:bg-[#F7F2E2] hover:duration-500 hover:ease-in-out ease-in-out duration-500 ">
-                <div className="flex justify-between">
-                  <h3
-                    className={`${
-                      food.role.length &&
-                      "font-bold text-sm bg-gray-800 rounded-xl px-3 text-white"
-                    }`}
-                  >
-                    {food?.role}
-                  </h3>
-                  <FaHeart className="text-gray-300 text-xl hover:text-red-500" />
-                </div>
-                <figure className="flex justify-center">
-                  <Image width={200} height={200} src={`${food.img}`} alt="" />
-                </figure>
-              </div>
-              <div className="p-5">
-                <h2 className="text-lg font-bold text-black py-2">
-                  {food.title}
-                </h2>
-                <p className="text-sm text-gray-500">{`${food.shortDescription.slice(
-                  0,
-                  69
-                )}`}</p>
-                <div className="flex justify-between items-center mt-6 pb-3">
-                  <p className="text-secondary text-lg font-extrabold">
-                    ${food.price}
-                  </p>
-                  <div className="bg-secondary p-3 rounded-xl">
-                    <BsBasket2Fill className="hover:text-white text-black" />
+      <div className="container mx-auto grid grid-cols-4 gap-8 mb-10">
+        {filterByBtn?.length >= 0
+          ? filterByBtn.map((food) => {
+              return (
+                <div key={food.id} className="rounded-3xl border">
+                  <div className="m-2 mb-0 px-3 pt-3 pb-7 rounded-3xl hover:bg-[#F7F2E2] hover:duration-500 hover:ease-in-out ease-in-out duration-500 ">
+                    <div className="flex justify-between">
+                      <h3
+                        className={`${
+                          food.role.length &&
+                          "font-bold text-sm bg-gray-800 rounded-xl px-3 text-white"
+                        }`}
+                      >
+                        {food?.role}
+                      </h3>
+                      <FaHeart className="text-gray-300 text-xl hover:text-red-500" />
+                    </div>
+                    <figure className="flex justify-center">
+                      <Image
+                        width={200}
+                        height={200}
+                        src={`${food.img}`}
+                        alt=""
+                      />
+                    </figure>
+                  </div>
+                  <div className="p-5">
+                    <h2 className="text-lg font-bold text-black py-2">
+                      {food.title}
+                    </h2>
+                    <p className="text-sm text-gray-500">{`${food.shortDescription.slice(
+                      0,
+                      69
+                    )}`}</p>
+                    <div className="flex justify-between items-center mt-6 pb-3">
+                      <p className="text-secondary text-lg font-extrabold">
+                        ${food.price}
+                      </p>
+                      <div className="bg-secondary p-3 rounded-xl">
+                        <BsBasket2Fill className="hover:text-white text-black" />
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          );
-        })}
+              );
+            })
+          : foods.map((food) => {
+              return (
+                <div key={food.id} className="rounded-3xl border">
+                  <div className="m-2 mb-0 px-3 pt-3 pb-7 rounded-3xl hover:bg-[#F7F2E2] hover:duration-500 hover:ease-in-out ease-in-out duration-500 ">
+                    <div className="flex justify-between">
+                      <h3
+                        className={`${
+                          food.role.length &&
+                          "font-bold text-sm bg-gray-800 rounded-xl px-3 text-white"
+                        }`}
+                      >
+                        {food?.role}
+                      </h3>
+                      <FaHeart className="text-gray-300 text-xl hover:text-red-500" />
+                    </div>
+                    <figure className="flex justify-center">
+                      <Image
+                        width={200}
+                        height={200}
+                        src={`${food.img}`}
+                        alt=""
+                      />
+                    </figure>
+                  </div>
+                  <div className="p-5">
+                    <h2 className="text-lg font-bold text-black py-2">
+                      {food.title}
+                    </h2>
+                    <p className="text-sm text-gray-500">{`${food.shortDescription.slice(
+                      0,
+                      69
+                    )}`}</p>
+                    <div className="flex justify-between items-center mt-6 pb-3">
+                      <p className="text-secondary text-lg font-extrabold">
+                        ${food.price}
+                      </p>
+                      <div className="bg-secondary p-3 rounded-xl">
+                        <BsBasket2Fill className="hover:text-white text-black" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
       </div>
       <div className="text-center pb-16">
         <button className="btn btn-secondary px-10">All Foods</button>
