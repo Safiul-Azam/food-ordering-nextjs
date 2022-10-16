ab001b177f62df6392035bc9cd63eb3db956562d
Safiul-Azam <safiulazamriad@gmail.com>
2022-10-16 12:36:33 +0600

    dishes menu for menu route #menu

---
+++ components/Menu/DishesMenu.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+// import PopularDishes from "./PopularDishes";
+
+function DishesMenu({ foods }) {
+  const [filterByBtn, setFilterByBtn] = useState([]);
+  const [active, setActive] = useState("all");
+
+  const btnByFoodName = [
+    { id: 1, name: "all" },
+    { id: 2, name: "pizza" },
+    { id: 3, name: "burger" },
+    { id: 4, name: "cold drink" },
+    { id: 5, name: "hot drink" },
+    { id: 6, name: "dessert" },
+  ];
+  const handleClick = (btn) => {
+    setFilterByBtn(foods.filter((food) => food.category.toLowerCase() === btn));
+    if (btn === "all") {
+      setFilterByBtn(foods);
+    }
+    // setFoodName(foods.find((food) => food?.category?.toLowerCase() === btn));
+    setActive(btn);
+  };
+  return (
+    <>
+      <div className="mt-16">
+        <h2 className="text-center text-4xl text-black font-extrabold mb-10">
+          Menus Of The Day
+        </h2>
+        <div className="flex justify-center space-x-10 mb-8">
+          {btnByFoodName.map((btn) => (
+            <div
+              className={`border flex justify-center hover:border-secondary w-28 p-2 rounded-3xl font-bold hover:duration-200 hover:ease-in-out ease-in-out duration-200 ${
+                active === btn.name ? "bg-secondary" : ""
+              }`}
+              key={btn.id}
+            >
+              <button
+                className="text-sm capitalize"
+                onClick={() => handleClick(btn.name)}
+              >
+                {btn.name}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* <PopularDishes filterByBtn={filterByBtn} foods={foods} /> */}
+    </>
+  );
+}
+
+export default DishesMenu;
--- pages/menu.js
+++ pages/menu.js
@@ -1,11 +1,13 @@
 import Banner from "../components/Menu/Banner";
+import DishesMenu from "../components/Menu/DishesMenu";
 
 function menu() {
-    return (
-        <div>
-           <Banner/>
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+      <DishesMenu />
+    </div>
+  );
 }
 
 export default menu;
