7f86aa7dac972ae6dc1a1e8ccbba4fe490dd9057
Safiul-Azam <safiulazamriad@gmail.com>
2022-10-19 19:04:27 +0600

    footer section is done #shared

--- components/shared/CopyRight.jsx
+++ components/shared/CopyRight.jsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../../images/icon/logo.png";
+import Footer from '../../images/banner/footer_img1.png'
 
 const CopyRight = () => {
   const today = new Date();
   const year = today.getFullYear();
   return (
     <div className="text-center bg-[#CE2829]">
-      <div className="text-white text-sm tracking-wide py-6 text-center">
-        <h4 className="text-lg text-white text-center">
+      <div className="text-white text-sm tracking-wide py-6 text-center flex container mx-auto justify-between">
+        <p className="text-white text-center">
           &copy; Copyright {year} by{" "}
-          <span className="text-secondary">SafiFood!</span>
-        </h4>
+          <span className="text-secondary">SafiFood!</span> All Rights Reserved.
+        </p>
+        <img src='https://i.ibb.co/GMHH1W3/footer-img1.png' alt="" />
       </div>
     </div>
   );
